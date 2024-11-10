0641c0618f9d4e45ce93275c3642dfd31f96c251
gentil-kadson <fernandes.gentilkadsonjac@gmail.com>
2024-11-10 15:45:52 -0300

    third test dataset implemented.

--- main.py
+++ main.py
@@ -1,60 +1,90 @@
 from classes import BinaryHeap, BinaryItem
 
-# 1ST DATASET
-first_heap_dataset = BinaryHeap()
+# # 1ST DATASET
+# first_heap_dataset = BinaryHeap()
 
-# 1.
-first_heap_dataset.insert(BinaryItem(10))
-first_heap_dataset.insert(BinaryItem(5))
-first_heap_dataset.insert(BinaryItem(20))
-first_heap_dataset.insert(BinaryItem(1))
-first_heap_dataset.insert(BinaryItem(15))
-first_heap_dataset.insert(BinaryItem(30))
-first_heap_dataset.insert(BinaryItem(25))
+# # 1.
+# first_heap_dataset.insert(BinaryItem(10))
+# first_heap_dataset.insert(BinaryItem(5))
+# first_heap_dataset.insert(BinaryItem(20))
+# first_heap_dataset.insert(BinaryItem(1))
+# first_heap_dataset.insert(BinaryItem(15))
+# first_heap_dataset.insert(BinaryItem(30))
+# first_heap_dataset.insert(BinaryItem(25))
 
-# 2.
-first_heap_dataset.change_priority(3, 50)
-first_heap_dataset.change_priority(1, 8)
+# # 2.
+# first_heap_dataset.change_priority(3, 50)
+# first_heap_dataset.change_priority(1, 8)
 
-# 3.
-first_heap_dataset.remove()
-first_heap_dataset.remove()
-first_heap_dataset.remove()
+# # 3.
+# first_heap_dataset.remove()
+# first_heap_dataset.remove()
+# first_heap_dataset.remove()
 
-# 4.
-first_heap_dataset.heap_sort()
+# # 4.
+# first_heap_dataset.heap_sort()
 
-# 5.
-first_heap_dataset.get_high_priority()
+# # 5.
+# first_heap_dataset.get_high_priority()
 
-# 2ND DATASET
-second_heap_dataset = BinaryHeap()
+# # 2ND DATASET
+# second_heap_dataset = BinaryHeap()
 
-# 1.
-second_heap_dataset.insert(BinaryItem(1))
-second_heap_dataset.insert(BinaryItem(2))
-second_heap_dataset.insert(BinaryItem(3))
-second_heap_dataset.insert(BinaryItem(4))
-second_heap_dataset.insert(BinaryItem(5))
-second_heap_dataset.insert(BinaryItem(6))
-second_heap_dataset.insert(BinaryItem(7))
-second_heap_dataset.insert(BinaryItem(8))
-second_heap_dataset.insert(BinaryItem(9))
-second_heap_dataset.insert(BinaryItem(10))
+# # 1.
+# second_heap_dataset.insert(BinaryItem(1))
+# second_heap_dataset.insert(BinaryItem(2))
+# second_heap_dataset.insert(BinaryItem(3))
+# second_heap_dataset.insert(BinaryItem(4))
+# second_heap_dataset.insert(BinaryItem(5))
+# second_heap_dataset.insert(BinaryItem(6))
+# second_heap_dataset.insert(BinaryItem(7))
+# second_heap_dataset.insert(BinaryItem(8))
+# second_heap_dataset.insert(BinaryItem(9))
+# second_heap_dataset.insert(BinaryItem(10))
 
-# 2.
-second_heap_dataset.change_priority(4, 15)
-second_heap_dataset.change_priority(8, 3)
+# # 2.
+# second_heap_dataset.change_priority(4, 15)
+# second_heap_dataset.change_priority(8, 3)
 
-# 3.
-second_heap_dataset.remove()
-second_heap_dataset.remove()
-second_heap_dataset.remove()
-second_heap_dataset.remove()
-second_heap_dataset.remove()
+# # 3.
+# second_heap_dataset.remove()
+# second_heap_dataset.remove()
+# second_heap_dataset.remove()
+# second_heap_dataset.remove()
+# second_heap_dataset.remove()
 
-# 4.
-second_heap_dataset.heap_sort()
+# # 4.
+# second_heap_dataset.heap_sort()
 
-# 5.
-second_heap_dataset.get_high_priority()
+# # 5.
+# second_heap_dataset.get_high_priority()
+
+# # 3RD DATASET
+# third_heap_dataset = BinaryHeap()
+
+# # 1.
+# third_heap_dataset.insert(BinaryItem(50))
+# third_heap_dataset.insert(BinaryItem(40))
+# third_heap_dataset.insert(BinaryItem(30))
+# third_heap_dataset.insert(BinaryItem(20))
+# third_heap_dataset.insert(BinaryItem(10))
+# third_heap_dataset.insert(BinaryItem(5))
+# third_heap_dataset.insert(BinaryItem(3))
+
+# # 2.
+# third_heap_dataset.change_priority(2, 60)
+# third_heap_dataset.change_priority(5, 1)
+
+# # 3.
+# third_heap_dataset.remove()
+# third_heap_dataset.remove()
+# third_heap_dataset.remove()
+
+# # 4.
+# third_heap_dataset.display_heap()
+# third_heap_dataset.heap_sort()
+
+# # 5.
+# third_heap_dataset.get_high_priority()
+
+# 4TH DATASET
